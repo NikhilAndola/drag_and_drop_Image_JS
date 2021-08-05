fb78487058f5aeae2df3a68019924240400e0db7
nikhilAndola <nikhilandola123@gmail.com>
2021-08-05 21:36:27 +0530

    drag and drop project completed

---
+++ js/index.js
@@ -0,0 +1,44 @@
+console.log("drag and drop utility");
+
+const imgBox = document.querySelector(".imgBox");
+const whiteBoxes = document.getElementsByClassName("whiteBox");
+
+// Event listener for draggable element imgBox 
+imgBox.addEventListener("dragstart", (e) => {
+    console.log("DragStart has been triggered");
+    e.target.className += ' hold';
+    setTimeout(() => {
+        e.target.className = 'hide';
+        
+    }, 0);
+});
+
+imgBox.addEventListener("dragend", (e) => {
+    console.log("DragEnd has been triggered");
+    e.target.className = "imgBox";
+});
+
+for(whiteBox of whiteBoxes){
+    whiteBox.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        console.log("DragOver has been triggered");
+    }) 
+
+    whiteBox.addEventListener('dragenter', (e) => {
+        console.log("DragEnter has been triggered");
+        e.target.className += ' dashed';
+    })
+
+    whiteBox.addEventListener('dragleave', (e ) => {
+        console.log("DragLeave has been triggered");
+        e.target.className = "whiteBox";
+    })
+
+    whiteBox.addEventListener('drop', (e) => {
+        console.log("drop has been triggered");
+        e.target.append(imgBox);
+    })
+}
+
+
+// console.log(whiteBox);
